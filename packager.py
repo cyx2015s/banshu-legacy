baef1631f62c4a434e9ef12921490a2e618292a6
cyx2015s <31377746+cyx2015s@users.noreply.github.com>
2025-01-11 23:58:14 +0800

    added packager and changelog

---
+++ packager.py
@@ -0,0 +1,20 @@
+import os
+import zipfile
+
+
+def package_files(output_filename="package.zip"):
+    ignored_files = {"packager.py", ".git", ".gitignore", output_filename}
+
+    with zipfile.ZipFile(output_filename, "w") as zipf:
+        for root, dirs, files in os.walk("."):
+            for file in files:
+                file_path = os.path.relpath(os.path.join(root, file), ".")
+                if not any(
+                    file_path.startswith(ignored_file) for ignored_file in ignored_files
+                ):
+                    print(f"Adding {file_path}")
+                    zipf.write(file_path)
+
+
+if __name__ == "__main__":
+    package_files("banshu_0.0.2.zip")
